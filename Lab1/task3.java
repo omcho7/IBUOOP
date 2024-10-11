66349ac7a7a01223002d058816fe0da47126f245
omcho7 <osmanovicomar@gmail.com>
2024-10-11 22:33:50 +0200

    Accidentaly added the wrong folders to gitignore, trying to push Lab2 again

---
+++ Lab1/task3.java
@@ -0,0 +1,6 @@
+public class task3 {
+    public static void main(String[] args) {
+        int secondsInYear = 365 * 24 * 60 * 60;
+        System.out.println("The number of seconds in a year (365 days) is " + secondsInYear + " seconds.");
+    }
+}
